39aaf9bd02baa909a364fff0415b4056a077e421
mariusgessler <marius.gessler@gmail.com>
2020-07-19 14:39:00 +0200

    Add readability.py

--- problem_sets/readability.py
+++ problem_sets/readability.py
@@ -1,17 +1,24 @@
-from cs50 import get_float
+from cs50 import get_string
 
-while True:
-    dollars = get_float("Change owed: ")
-    if dollars >= 0:
-        break
+text = get_string('Enter text: ').strip()
+num_words, num_letters, num_sentences = 0,0,0
 
-cents = round(dollars * 100)
-denominations = [25, 10, 5, 1]
-coins = 0
+for i in range(len(text)):
+    if (i == 0 and text[i] != ' ') or (i != len(text) - 1 and text[i] == ' ' and text[i + 1] != ' '):
+        num_words += 1
+    if text[i].isalpha():
+        num_letters += 1
+    if text[i] == '.' or text[i] == '?' or text[i] == '!':
+        num_sentences += 1
 
-for item in denominations:
-    while cents >= item:
-        cents -= item
-        coins += 1
+# Coleman-Liau formula
+L = num_letters/ num_words * 100
+S = num_sentences / num_words * 100
+index = round(0.0588 * L - 0.296 * S - 15.8)
 
-print(coins)
+if index < 1:
+    print("Before Grade 1")
+elif index >= 16:
+    print("Grade 16+")
+else:
+    print("Grade", index)
